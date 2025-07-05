52cf4f20bdd8d0af0f83fd9120a2811eae88eb0c
PsuedoBlessings <zmasike@gmail.com>
2025-07-04 17:29:54 -0700

    Database Function "deleteSocialAccount" has been created and tested. Also added a new section/describe block named 'Database Social Accounts Table Functionality'

--- src/test/db/db.test.ts
+++ src/test/db/db.test.ts
@@ -1,4 +1,4 @@
-import {initializeDatabase, createTables, addSocialAccount} from '../../main/db/index';
+import {initializeDatabase, createTables, addSocialAccount, deleteSocialAccount} from '../../main/db/index';
 import fs from 'fs';
 import {Database} from 'sqlite3';
 import crypto from 'crypto';
@@ -39,18 +39,43 @@ test('Create Tables Test', (done) => {
     });
 });
 
-test('Adding Social Account to Database', (done) => {
-    addSocialAccount(db, 'testuser', 'testpassword').then((account_id) => {
-        expect(account_id).toBeDefined();
-        db.get(`SELECT * FROM "Social Accounts" WHERE account_id = ?`, [account_id], (err, row:any) => {
-        expect(err).toBeNull();
-        expect(row).toBeDefined();
-        expect(row.account_id).toBe(account_id);
-        expect(row.username).toBe('testuser');
-        expect(row.password).toBe('testpassword');
-        done();
+describe('Database Social Accounts Table Functionality', () => {
+    beforeEach(()=>{
+        // Ensure the "Social Accounts" table is empty before each test
+        db.run(`DELETE FROM "Social Accounts"`, [], (err) => {
+            expect(err).toBeNull();
+        });
     });
-    }).catch((err) => {
-        console.error("Error adding social account:", err);
+    test('Adding Social Account to Database', (done) => {
+        addSocialAccount(db, 'testuser', 'testpassword').then((account_id) => {
+            expect(account_id).toBeDefined();
+            db.get(`SELECT * FROM "Social Accounts" WHERE account_id = ?`, [account_id], (err, row:any) => {
+            expect(err).toBeNull();
+            expect(row).toBeDefined();
+            expect(row.account_id).toBe(account_id);
+            expect(row.username).toBe('testuser');
+            expect(row.password).toBe('testpassword');
+            done();
+        });
+        }).catch((err) => {
+            console.error("Error adding social account:", err);
+        });
+    });
+
+    test('Deleting Social Account from Database', (done) => {
+        let account_id: any;
+        addSocialAccount(db, 'testuser', 'testpassword').then((id) => {
+            account_id = id;
+            return deleteSocialAccount(db, account_id);
+        }).then(() => {
+            db.get(`SELECT * FROM "Social Accounts" WHERE account_id = ?`, [account_id], (err, row:any) => {
+                expect(err).toBeNull();
+                expect(row).toBeUndefined();
+                done();
+            });
+        }).catch((err) => {
+            console.error("Error deleting social account:", err);
+        });
     });
 });
+
