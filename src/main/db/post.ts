895e07dfa92ad946aa8e8c0101361738d7ccb807
PsuedoBlessings <zmasike@gmail.com>
2025-08-13 15:27:46 -1000

    Added Database Function "addPost"

--- src/main/db/post.ts
+++ src/main/db/post.ts
@@ -1,3 +1,4 @@
+import { Data } from "electron";
 import { Database } from "sqlite/build";
 
 export interface PostInterface {
@@ -7,5 +8,27 @@ export interface PostInterface {
     author: string;
     description?: string;
     timestamp: Date;
-    media_url: string;
+    media_urls: string;
+}
+
+export function addPost(db:Database,  new_post:Partial<PostInterface>): Promise<PostInterface>{
+        return new Promise((resolve, reject)=>{
+            const sql = `INSERT INTO Posts (post_id, account_id, platform_id, author, description, timestamp, media_urls) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+            db.run(sql, [new_post.post_id, new_post.account_id, new_post.platform_id, new_post.author, new_post.description ?? null, new_post.timestamp, new_post.media_urls], (err:Error) =>{
+                if(err){
+                    reject(err);
+                }
+                else{
+                    const sql = `SELECT * FROM Posts WHERE post_id = ?`
+                    db.get(sql, [new_post.post_id], (err:Error, row:PostInterface) =>{
+                        if(err){
+                            reject(err)
+                        }
+                        else{
+                            resolve(row);
+                        }
+                    });
+                }
+        });
+    });
 }
