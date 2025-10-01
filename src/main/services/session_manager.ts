d99397c1a9badcb934fd9fdcf9d1e81d3319a0a8
PsuedoBlessings <zmasike@gmail.com>
2025-09-30 23:10:49 -0700

    Added session manager function "getName"

--- src/main/services/session_manager.ts
+++ src/main/services/session_manager.ts
@@ -20,3 +20,26 @@ export function loadSession(session_name:string):Promise<Session>{
         }
     });
 }
+
+/*
+Name: getName
+Parameter: ses (Session)
+Description:
+    This fucntion will get the name of the session. The name of the session is discovered through the file path of the session.
+ */
+export function getName(ses:Session):Promise<String>{
+    return new Promise((resolve, reject) => {
+        try{
+            const storage_path:String = ses.getStoragePath();
+            const session_name:String = storage_path.match(/Partitions[/\\]([^/\\]+)$/)[1];
+            if(session_name === undefined){
+                console.error('Error Session Name doesnt exist');
+                reject();
+            }
+            resolve(session_name);
+        }catch(err){
+            console.log("Error getting session name:", err);
+            reject();
+        }
+    });
+}
