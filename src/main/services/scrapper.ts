965eca05c62bc3322caa807e9c00da9f8612dd05
PsuedoBlessings <zmasike@gmail.com>
2025-10-03 01:23:33 -0700

    Uploaded both cheerio and xpath for scrapping. Added scrapper function "extractParent"

--- src/main/services/scrapper.ts
+++ src/main/services/scrapper.ts
@@ -1,14 +1,29 @@
 import * as cheerio from 'cheerio';
+import * as xpath from 'xpath';
+import * as xpath_doc from '@xmldom/xmldom';
 import { BrowserWindow } from 'electron';
 
-export async function extractDOM(window:BrowserWindow):Promise<Node>{
+type SelectorType = "CSS" | "Xpath";
+
+export async function extractDOM(window:BrowserWindow):Promise<string>{
     const contents = window.webContents;
     try{
-        let DOM:Node = await contents.executeJavaScript('document.documentElement.outerHTML', true);
+        let DOM:string = await contents.executeJavaScript('document.documentElement.outerHTML', true);
         console.log('DOM Extracted:', DOM);
         return DOM;
     }catch(error){
         console.error('Error Extracting Dom:', error);
+        throw error;
+    }
+}
+
+export async function extractParent(DOM: string, selector: string): Promise<cheerio.Cheerio<string>> {
+    try {
+        const $DOM: cheerio.CheerioAPI = cheerio.load(DOM);
+        const $parent_element: cheerio.Cheerio<string> = $DOM(selector);
+        return $parent_element;
+    } catch (error) {
+        console.error('Error extracting parent:', error);
         throw error;
     }
 }
